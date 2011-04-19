0477e1b27e2f36a983fa9e2b60e36a6a23515089
Chris McMahan <cmcmahan@gmail.com>
2011-04-18 21:39:34 -0400

    updated org agenda templates

--- emacs-org.el
+++ emacs-org.el
@@ -20,21 +20,11 @@
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
-;;; load org contacts (bbdb replacement!)
-;(require 'org-contacts)
-
 ;;;_.============================================================
 ;;;_. Set up some faces
 (make-face 'org-hide)
 (set-face-foreground 'org-hide "#343434")
 (setq org-hide-leading-stars t)
-
-;;;_.============================================================
-;;; Org contacts
-
-;;; set the org-contacts file
-;(custom-set-variables
-;'(org-contacts-files '("~/org/contacts.org")))
 
 ;;;_.============================================================
 ;;;_. capture templates (replaces remember)
@@ -65,48 +55,18 @@
         ("p" "Password" entry
          (file+olp "~/org/passwords.gpg" "Passwords")
          "* %^{Title}\n  :PROPERTIES:\n  :Update:   %u\n  :Url:      %^{Url}p\n  :Username: %^{Username}p\n  :Password: %^{Password}p\n  :Notes:    %^{Notes}p\n  :END:\n")))
-;        ("c" "Contacts" entry (file "~/Org/contacts.org")
-;          "** %(org-contacts-template-name)
-; :PROPERTIES:
-; :Email: %(org-contacts-template-email)
-; :END:")))
-
-;; Great template example from Sacha 
-;; Uses two levels of prompts in the capture
-;;
-;; (setq org-capture-templates
-;;       (append '(("l" "Ledger entries")
-;;                 ("lm" "MBNA" plain
-;;                  (file "~/personal/ledger")
-;;                  "%(org-read-date) %^{Payee}
-;;   Liabilities:MBNA
-;;   Expenses:%^{Account}  %^{Amount}
-;; ")
-;;                 ("lc" "Cash" plain
-;;                 (file "~/personal/ledger")
-;; 	        "%(org-read-date) * %^{Payee}
-;;   Expenses:Cash
-;;   Expenses:%^{Account}  %^{Amount}
-;; "))
-;;        org-capture-templates))
-
-
 
 ;;;_.============================================================
 ;;;_. various settings
 (setq org-agenda-include-diary t
       org-agenda-remove-tags t
       org-agenda-include-all-todo t
+      org-agenda-tags-column -100
+      org-tags-column -100
       org-use-fast-todo-selection t
       org-confirm-elisp-link-function `y-or-n-p
       org-attach-directory "/Users/cmcmahan/org/data/")
-
-; set the column for tags. Negative number right-justifies on that
-; column
-(setq org-agenda-tags-column -100)
-(setq org-tags-column -100)
-
-
+ 
 ;;;_.============================================================
 ;;;_. Set up org files
 (setq org-agenda-files 
@@ -127,52 +87,55 @@
 ;;;_.============================================================
 ;;;_. Set up the global tags list
 (setq org-tag-alist 
-      '(
-        ("Appt"       . ?a)
+      '(("Appt"       . ?a)
         ("Emacs"      . ?e)
         ("KMA"        . ?k)
         ("Navy"       . ?n)
         ("NACRA"      . ?N)
         ("Personal"   . ?p)
-        ("Reference"  . ?r)))
+        ("Reference"  . ?r)
+        ("Xetron"     . ?x)))
 
 ;;;_.============================================================
 ;;;_. Create custom agenda views
+;; good resource is 
+;; http://orgmode.org/worg/org-tutorials/org-custom-agenda-commands.html
+
+
+(setq org-agenda-block-separator 
+      "===================================================================")
+(setq org-agenda-current-time-string
+      "now - - - - - - - - - - - - - -")
+
 (setq org-agenda-custom-commands
- '(("d" "Daily Agenda"
-    ((agenda ""
-             ((org-agenda-todo-keyword-format "%-11s")
-              (org-agenda-remove-tags t)))))
-;     (todo "OPEN" 
-;           ((org-agenda-sorting-strategy '(tag-up))
-;            (org-agenda-show-inherited-tags nil)
-;            (org-agenda-todo-keyword-format "")))))
-;     (todo "OPEN"
-;           ((org-agenda-todo-keyword-format "")))
-;     (todo "WAITING"
-;           ((org-agenda-todo-keyword-format "")))))
-   ("g" "GeekTool Agenda"
-    ((agenda ""))
-    ((org-agenda-todo-keyword-format "%-11s")
-     (org-agenda-prefix-format "  %-8T%?-16t% s")
-     (org-agenda-show-inherited-tags nil)
-     (org-agenda-remove-tags 'prefix)
-     (org-agenda-tags-column 70))
-    ("~/.org-agenda.txt"))
-   ("c" "Calendar" agenda ""
-    ((org-agenda-ndays 7)
-     (org-agenda-start-on-weekday 0) ; start on Sunday
-     (org-agenda-time-grid nil)
-     (org-agenda-entry-types '(:timestamp :sexp))
-     (org-agenda-prefix-format " %-12:t ")
-     (org-deadline-warning-days 0)
-     (org-agenda-include-all-todo nil)
-     (org-agenda-repeating-timestamp-show-all t)
-     (org-agenda-filter-preset '("-nocal1"))
-     (org-agenda-hide-tags-regexp ".*")
-     ))))
-
-
+      '(("d" "Agenda and all tasks"
+         ((agenda)
+          (todo "OPEN")
+          (todo "NEXT")))
+        ("n" "Agenda and Navy-related tasks"
+         ((agenda)
+          (tags-todo "Navy")))
+        ("x" "Agenda and Xetron-related tasks"
+         ((agenda)
+          (tags-todo "xetron")))
+        ("o" "Agenda and all open tasks"
+         ((agenda)
+          (todo "OPEN")))
+        ("c" "Calendar Agenda"
+         ((agenda)
+          (todo))
+         ((org-agenda-ndays 7)
+          (org-agenda-remove-tags t)
+          (org-agenda-start-on-weekday 1) ; start on Monday
+          (org-agenda-entry-types '(:timestamp :sexp))
+          (org-agenda-prefix-format " %-12:t ")
+          (org-deadline-warning-days 0)
+          (org-agenda-include-all-todo nil)
+          (org-agenda-repeating-timestamp-show-all t)
+          (org-agenda-filter-preset '("-nocal1"))
+          (org-agenda-hide-tags-regexp ".*"))
+         ("~/.org-agenda.txt"))
+        ))
 
 ;; To use the GeekTool agenda from a command line, put the following
 ;; in the shell command field. Note, emacs server must be running for
