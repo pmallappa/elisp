cb0b93f98c1470d95793bf1ce91b28ebaf9a45c8
Chris McMahan <cmcmahan@gmail.com>
2014-05-15 19:58:42 -0400

    installed helm-is-git

--- emacs-helm.el
+++ emacs-helm.el
@@ -1,8 +1,6 @@
 (add-to-list 
  'load-path (expand-file-name (concat EMACS_PKGS "/helm")))
 
-(require 'helm-config)
-
 (require 'helm-config) 
 (setq helm-candidate-number-limit 10)
 ;; From https://gist.github.com/antifuchs/9238468
@@ -23,3 +21,29 @@
 (global-set-key (kbd "C-c h") 'helm-mini)
 
 (ido-mode -1) ;; Turn off ido mode in case I enabled it accidentally
+
+
+
+(require 'helm-locate)
+(require 'helm-git)g
+(require 'helm-c-moccur)
+(helm-mode 1)
+(helm-match-plugin-mode 1)
+(helm-adaptative-mode 1)
+
+(setq helm-idle-delay 0.0
+      helm-quick-update t
+      helm-su-or-sudo "sudo"
+      helm-ff-transformer-show-only-basename nil
+      helm-mp-matching-method 'multi2
+      helm-M-x-requires-pattern 0
+      )
+
+(global-set-key (kbd "C-c f") 'helm-for-files)
+(global-set-key (kbd "C-;") 'helm-git-find-files)
+
+
+
+
+
+(provide 'emacs-helm)
