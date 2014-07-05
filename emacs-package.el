1f353e7408b5101d7561f932c1da54b2c64d5ca9
Chris McMahan <cmcmahan@gmail.com>
2014-07-04 20:22:52 -0400

    Interactive query if packages are missing

--- emacs-package.el
+++ emacs-package.el
@@ -1,27 +1,46 @@
-  (require 'package)
-  (add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/") t)
+;; emacs package manager.
+(require 'package)
+(add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/") t)
 ;  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-  (package-initialize)
+(package-initialize)
 
-(when (eq 0 (length package-alist))
-      (package-refresh-contents)
-      (dolist
-	  (pkg  '(clojure-mode
- 		    clojure-test-mode
-		    cider
-		    company
-		    company-cider
-		    w3m
-		    bm
-		    bs-ext
-		    exec-path-from-shell
-		    helm
-		    igrep
-		    magit
-		    org
-		    s
-		    w3m))
-	(unless (package-installed-p pkg)
-	  (package-install pkg))))
+;;; Required packages
+;;; everytime emacs starts, it will automatically check if those packages
+;;; are missing, it will install them automatically
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(defvar cm/packages
+  '(bm
+    bs-ext
+    color-moccur
+    csv-mode
+    dash
+    epl
+    exec-path-from-shell
+    git-commit-mode
+    git-rebase-mode
+    igrep
+    magit
+    org
+    pkg-info
+    s
+    starter-kit-eshell
+    sublime-themes
+    tangotango-theme
+    tree-mode
+    w3m
+    windata
+    zenburn-theme)
+  )
+
+(dolist (p cm/packages)
+      (if (not (package-installed-p p))
+          (progn
+            (if (y-or-n-p (format "%s: %s " "install missing package" p))
+                (progn 
+                  (package-install p)
+                  (require p))
+              nil))
+        (require p)))
 
 (provide 'emacs-package)
