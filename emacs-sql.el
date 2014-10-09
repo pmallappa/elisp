b44992661b33462038cc6b82a2fc4ad879a26abb
Chris McMahan <christopher.mcmahan@siemens.com>
2014-10-09 17:08:29 -0400

    tweaks to sqlplus fonts

--- emacs-sql.el
+++ emacs-sql.el
@@ -24,6 +24,18 @@
 ;; e.g. sales/paSSW0rd@localhost:21521/webtst
 
 (require 'org-table)
+;; don't use adjacent tables, present results as expected
+(setq sqlplus-multi-output-tables-default-flag t)
+
+;; adjust the table faces to lighten the background up a bit
+;; default was -20 and -30
+;; note, required changing the source code sqlplus.el to work, commented out lines 3385-3398
+;; will send a change proposal to the author
+(set-face-background 'sqlplus-table-even-rows-face (sqlplus-shine-color (face-background 'default) -10))
+(set-face-background 'sqlplus-table-odd-rows-face (sqlplus-shine-color (face-background 'default) -20))
+
+;; Use org table to store connection info.
+;; See the password.gpg file for current connections
 (defvar sqlplus-x-columns '(sqlplus-x-service sqlplus-x-user sqlplus-x-pwd))
 (defun sqlplus-x-connect ()
   "Build a connection string and make a connection. The point must be in an org-mode table.
