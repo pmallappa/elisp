51729ca3ee8031777a6329196a60e654e101a24b
Chris McMahan <christopher.mcmahan@siemens.com>
2014-10-31 15:56:48 -0400

    Points to setup-cygwin for nt, settings for cygwin emacs

--- emacs-cygwin.el
+++ emacs-cygwin.el
@@ -1,43 +1,49 @@
 ;;;======================================================================
-(require 'cygwin-mount)
-(cygwin-mount-activate)
+(if (eq system-type 'windows-nt)
+    (progn
+      (require 'setup-cygwin)))
 
-;; This assumes that Cygwin is installed in C:\cygwin (the
-;; default) and that C:\cygwin\bin is not already in your
-;; Windows Path (it generally should not be).
+(if (eq system-type 'cygwin)
+    (progn
+      ;; from http://superuser.com/questions/229544/running-emacs-in-cygwin
 
-(setq exec-path (cons "C:/cygwin/bin" exec-path))
-(setenv "PATH" (concat "C:\\cygwin\\bin;" (getenv "PATH")))
+      ;; If you compile java or other things that generate filenames like
+      ;; C:\whatever, you may first like to edit /etc/fstab and add a mount
+      ;; from C:\ to /c such as:
+      ;; C: /c ntfs binary,user 1 1
 
-;;   LOGNAME and USER are expected in many Emacs packages
-;;   Check these environment variables.
+      ;; Then if you run emacs -nw inside mintty, you may like it to
+      ;; recognize more keys, place into ~/.emacs:
 
-(if (and (null (getenv "USER"))
-	 ;; Windows includes variable USERNAME, which is copied to
-	 ;; LOGNAME and USER respectively.
-	 (getenv "USERNAME"))
-    (setenv "USER" (getenv "USERNAME")))
+      ;; ;; For mintty
+      ;; (define-key function-key-map "\e[1;5m" [(control ?-)])
+      ;; (define-key function-key-map "\e[1;5k" [(control ?=)])
+      ;; (define-key function-key-map "\e[1;5q" [(control ?1)])
+      ;; (define-key function-key-map "\e[1;5s" [(control ?3)])
+      ;; (define-key function-key-map "\e[1;5t" [(control ?4)])
+      ;; (define-key function-key-map "\e[1;5u" [(control ?5)])
+      ;; (define-key function-key-map "\e[1;5w" [(control ?7)])
+      ;; (define-key function-key-map "\e[1;5x" [(control ?8)])
+      ;; (define-key function-key-map "\e[1;5y" [(control ?9)])
+      ;; (define-key function-key-map "\e[1;5p" [(control ?0)])
+      
+      ;; When in cygwin, allow C:\whatever to turn into /c/whatever
+      (defun cygwin-name-hook (operation &rest args)
+        "Turn Windows filenames into Cygwin filenames."
+        ;; Handle all operations the same
+        (let ((first (car args))
+              (inhibit-file-name-handlers
+               (cons 'cygwin-name-hook
+                     (and (eq inhibit-file-name-operation operation)
+                          inhibit-file-name-handlers)))
+              (inhibit-file-name-operation operation))
+          (setq first (replace-regexp-in-string "^C:" "/c" first t))
+          (setq first (replace-regexp-in-string "\\\\" "/" first t))
+          (apply operation (cons first (cdr args)))))
 
-(if (and (getenv "LOGNAME")
-	 ;;  Bash shell defines only LOGNAME
-	 (null (getenv "USER")))
-    (setenv "USER" (getenv "LOGNAME")))
-
-(if (and (getenv "USER")
-	 (null (getenv "LOGNAME")))
-    (setenv "LOGNAME" (getenv "USER")))
-
-;;============================================================
-;; (A) M-x shell: This change M-x shell permanently
-;; Would call Windows command interpreter. Change it.
-
-(setq shell-file-name "bash")
-(setenv "SHELL" shell-file-name)
-(setq explicit-shell-file-name shell-file-name)
-
-;; Remove C-m (^M) characters that appear in output
-(add-hook 'comint-output-filter-functions
-          'comint-strip-ctrl-m)
+      (add-to-list 'file-name-handler-alist '("^[Cc]:" . cygwin-name-hook))      
+      ))
+    
 
 
 (provide 'emacs-cygwin)
