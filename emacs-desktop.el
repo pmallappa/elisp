26b2e85f278ed8d52b67eca08a0d42904f484b2c
Chris McMahan <christopher.mcmahan@siemens.com>
2014-12-04 16:10:03 -0500

    Added desktop support

---
+++ emacs-desktop.el
@@ -0,0 +1,12 @@
+;; Desktop mode saves the state of emacs from one session to another
+
+;; you can save multiple desktops by using different directories, and
+;; specifying the directory name in the call (desktop-read dirname).
+(setq desktop-path '("~/.emacs.d/desktops/sql"))
+(setq desktop-base-file-name ".emacs.desktop")
+(setq desktop-auto-save-timer nil)
+
+;; save desktop manually by calling `desktop-save'
+(desktop-save-mode nil)
+
+(provide 'emacs-desktop)
