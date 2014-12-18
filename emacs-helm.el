aaec628913f9a4d59be4399ee2c58338ee460e39
Chris McMahan <christopher.mcmahan@siemens.com>
2014-12-18 17:08:45 -0500

    minor Helm updates

--- emacs-helm.el
+++ emacs-helm.el
@@ -1,24 +1,28 @@
 ;; Helm configuration
+
 ;; good documentation sources
+;; ==============================
 ;; https://tuhdo.github.io/helm-intro.html
 ;; http://www.xiaohanyu.me/oh-my-emacs/core/ome-completion.html
+;; http://tuhdo.github.io/helm-intro.html
 ;; http://tuhdo.github.io/helm-projectile.html
 
-;; I remapped the helm-command key to C-c h (vice C-x c, which is too close to C-x C-c)
-;; C-x c l: helm-locate
-;; C-x c /: helm-find
-;; C-x c f: helm-for-files
-;; C-x c M-x: helm-M-x
-;; C-x c a: helm-apropos
-;; C-x c r: helm-regexp
-;; C-x c c: helm-colors
-;; C-x c 8: helm-ucs
-;; C-x c i: helm-imenu
-;; C-x c m: helm-man-woman
-;; C-x c t: helm-top
-;; C-x c p: helm-list-emacs-process
-;; C-x c M-y: helm-show-kill-ring
-
+;; I remapped the helm-command key to C-c h (vice C-x c, which is too close
+;; to C-x C-c)
+;; ==============================
+;; C-c h l: helm-locate
+;; C-c h /: helm-find
+;; C-c h f: helm-for-files
+;; C-c h M-x: helm-M-x
+;; C-c h a: helm-apropos
+;; C-c h r: helm-regexp
+;; C-c h c: helm-colors
+;; C-c h 8: helm-ucs
+;; C-c h i: helm-imenu
+;; C-c h m: helm-man-woman
+;; C-c h t: helm-top
+;; C-c h p: helm-list-emacs-process
+;; C-c h M-y: helm-show-kill-ring
 
 ;; enable helm universally
 (require 'helm-config)
