8af30ed20382569cfcf45e12b2633609e6e24f13
Chris McMahan <christopher.mcmahan@siemens.com>
2015-12-09 16:53:14 -0500

    Playing with Swiper

--- emacs-custom.el
+++ emacs-custom.el
@@ -7,7 +7,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (zenburn-theme yasnippet xkcd worf web-mode w3m swiper-helm smartparens s rainbow-mode pkg-info ox-pandoc orgit org-pandoc multi-web-mode magit-find-file js2-mode hl-sexp highlight-symbol gitconfig-mode git-timemachine git-gutter-fringe frame-cmds faff-theme esh-help diminish darktooth-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-moccur browse-url-dwim bookmark+ bm bind-key base16-theme autumn-light-theme auto-complete atom-one-dark-theme ample-zen-theme alect-themes aggressive-indent))))
+    (zenburn-theme yasnippet xkcd worf web-mode w3m smartparens s rainbow-mode pkg-info ox-pandoc orgit org-pandoc multi-web-mode magit-find-file js2-mode hl-sexp highlight-symbol gitconfig-mode git-timemachine git-gutter-fringe frame-cmds faff-theme esh-help diminish darktooth-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized color-moccur browse-url-dwim bookmark+ bm bind-key base16-theme autumn-light-theme auto-complete atom-one-dark-theme ample-zen-theme alect-themes aggressive-indent))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- emacs-org.el
+++ emacs-org.el
@@ -114,12 +114,13 @@
 
 ;;============================================================
 ;; default link description to only the filename
-(defun org-link-describe (link desc)
-  (if (file-exists-p link)
-      desc
-    (read-string "Description: " (file-name-nondirectory desc))))
-
-(setf org-make-link-description-function #'org-link-describe)
+;; broke when creating link from inside a file... needs work
+;(defun org-link-describe (link desc)
+;  (if (file-exists-p link)
+;      desc
+;    (read-string "Description: " (file-name-nondirectory desc))))
+;
+;(setf org-make-link-description-function #'org-link-describe)
 
 ;;============================================================
 ;; various settings
@@ -487,6 +488,48 @@ Return output file name."
                         (call-interactively 'show-branches)
                         (worf-more)))
             (pattern-transformer . worf--pattern-transformer)))))
+
+
+;;; Remove from agenda time grid lines that are in an appointment
+;;; from Michael Ekstrand at http://orgmode.org/worg/org-hacks.html
+;(defun org-time-to-minutes (time)
+;  "Convert an HHMM time to minutes"
+;  (+ (* (/ time 100) 60) (% time 100)))
+;
+;(defun org-time-from-minutes (minutes)
+;  "Convert a number of minutes to an HHMM time"
+;  (+ (* (/ minutes 60) 100) (% minutes 60)))
+;
+;(defadvice org-agenda-add-time-grid-maybe (around mde-org-agenda-grid-tweakify
+;                                                  (list ndays todayp))
+;  (if (member 'remove-match (car org-agenda-time-grid))
+;      (flet ((extract-window
+;              (line)
+;              (let ((start (get-text-property 1 'time-of-day line))
+;                    (dur (get-text-property 1 'duration line)))
+;                (cond
+;                 ((and start dur)
+;                  (cons start
+;                        (org-time-from-minutes
+;                         (+ dur (org-time-to-minutes start)))))
+;                 (start start)
+;                 (t nil)))))
+;        (let* ((windows (delq nil (mapcar 'extract-window list)))
+;               (org-agenda-time-grid
+;                (list (car org-agenda-time-grid)
+;                      (cadr org-agenda-time-grid)
+;                      (remove-if
+;                       (lambda (time)
+;                         (find-if (lambda (w)
+;                                    (if (numberp w)
+;                                        (equal w time)
+;                                      (and (>= time (car w))
+;                                           (< time (cdr w)))))
+;                                  windows))
+;                       (caddr org-agenda-time-grid)))))
+;          ad-do-it))
+;    ad-do-it))
+;(ad-activate 'org-agenda-add-time-grid-maybe)
 
 (provide 'emacs-org)
 
